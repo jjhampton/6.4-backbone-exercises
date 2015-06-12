aff0e5dbb52d05182a1f8628aff65f2ec0db4438
John Hampton <john_j_hampton@yahoo.com>
2015-06-11 20:55:27 -0400

    Refactor to include preventDefault and set correct attributes in model definition

--- scripts/a/a.js
+++ scripts/a/a.js
@@ -3,21 +3,21 @@ import Blogpost from './models/blogpost';
 (function(){
   'use strict';
 
-  var blogpost = new Blogpost();
-  alert(blogpost.urlRoot);
-
   var addPost = function() {
+    var blogpost = new Blogpost();
     var title = $(".form-input-title").val();
     var body = $(".form-input-body").val();
     blogpost.save({
       title: title,
       body: body
-    });
+    }, {dataType: 'text', success: function(){console.log("success");}, error: function(){console.log("errror");}});
   };
 
-
-
   $(".outer-container").html(JST.formblogpost());
-  $(".form-post").on("submit", addPost);
+  $(".form-post").on("submit", function() {
+    event.preventDefault();
+    console.log('clicked');
+    addPost();
+    });
 
 })();
--- scripts/a/models/blogpost.js
+++ scripts/a/models/blogpost.js
@@ -1,5 +1,7 @@
 export default Backbone.Model.extend({
   idAttribute: '_id',
-  created_at: new Date(),
-  urlRoot: 'http://tiny-lasagna-server.herokuapp.com/collections/posts'
+  urlRoot: 'http://tiny-lasagna-server.herokuapp.com/collections/posts',
+  defaults: {
+    created_at: new Date()
+  }
 });
