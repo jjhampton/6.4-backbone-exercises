5fbfa3aebbe82275ef9b4dc6256c4b94c7615ff1
John Hampton <john_j_hampton@yahoo.com>
2015-06-12 16:06:44 -0400

    Define PostModel and PostsCollection constructors Import views and models into router Test collection.fetch() on blog post data successfully in router initialize

---
+++ scripts/c/models/postModel.js
@@ -0,0 +1,11 @@
+var PostModel = Backbone.Model.extend({
+  idAttribute: '_id'
+  //MAY NEED DEFAULTS, ADD IF NECESSARY
+});
+
+var PostsCollection = Backbone.Collection.extend({
+  model: PostModel,
+  url: 'http://tiny-lasagna-server.herokuapp.com/collections/posts'
+});
+
+export default {PostModel, PostsCollection};
--- scripts/c/router.js
+++ scripts/c/router.js
@@ -1,3 +1,10 @@
+import PostTitleListView from './views/postTitleListView';
+
+import {PostModel} from './models/postModel';
+import {PostsCollection} from './models/postModel';
+
+
+
 var Router = Backbone.Router.extend({
   routes: {
     '': 'index',
@@ -5,7 +12,9 @@ var Router = Backbone.Router.extend({
   },
 
   initialize: function() {
-
+    this.posts = new PostsCollection();
+    this.posts.fetch();
+    console.log(this.posts);
   },
 
   index: function() {
