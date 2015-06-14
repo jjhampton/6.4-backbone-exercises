eaed89eb5c17f9e01879d9201e2f87b5fdafd275
John Hampton <john_j_hampton@yahoo.com>
2015-06-14 12:48:50 -0400

    Remove unneeded event handlers and callback properties from PostTitleListView

--- scripts/c/views/postTitleListView.js
+++ scripts/c/views/postTitleListView.js
@@ -3,10 +3,6 @@ export default Backbone.View.extend({
   className: 'post-list',
   template: JST.c.sidebarPostTitleList,
 
-  events: {
-    'click .post-title-list-item': 'dispatcher'
-  },
-
   initialize: function() {
     this.render();
     this.listenTo(this.collection, 'update', this.render);
@@ -14,21 +10,5 @@ export default Backbone.View.extend({
 
   render: function() {
     this.$el.html(this.template(this.collection.toJSON()));
-  },
-
-  dispatcher: function(event) {
-    //gets post ID data by passing in click event parameter,
-    //then passes that value to setViewModelID to set postID value on model
-    this.setViewModelID(this.getPostData(event));
-  },
-
-  getPostData: function(event) {
-    var target = event.currentTarget;
-    var postID = target.dataset.index;
-    return postID;
-  },
-
-  setViewModelID: function(option) {
-    this.model.set({postID: option});
   }
 });
