18baae12bd7efbac8d44fdf24a329d0c3466eeda
John Hampton <john_j_hampton@yahoo.com>
2015-06-14 14:39:07 -0400

    Create initial static mockup for App D, linking d.js to index.html and alerting

---
+++ scripts/d/d.js
@@ -0,0 +1,13 @@
+// import router from './router';
+
+(function() {
+  'use strict';
+  alert("YO");
+  $(document).ready(function() {
+    // Backbone.history.start()
+
+  });
+
+})();
+
+// window.appRouter = new Router {}; - discussion idea from Jess
