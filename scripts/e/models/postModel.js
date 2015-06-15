3f97536994588ad27220eb4acacf213489439fc6
John Hampton <john_j_hampton@yahoo.com>
2015-06-15 00:45:40 -0400

    Add Jake's comparator method to PostsCollection - only adding new posts as second on list - need refactoring

--- scripts/e/models/postModel.js
+++ scripts/e/models/postModel.js
@@ -10,7 +10,13 @@ var PostModel = Backbone.Model.extend({
 
 var PostsCollection = Backbone.Collection.extend({
   model: PostModel,
-  url: 'http://tiny-lasagna-server.herokuapp.com/collections/posts'
+  url: 'http://tiny-lasagna-server.herokuapp.com/collections/posts',
+
+  //comparator credit: Jacob Smith: https://github.com/TIY-GVL-FEE-2015-May/6.4-image-board/blob/master/scripts/models/image.js
+  //Needs refactoring or change to other parts of program, possibly collection.create {wait:true} option?... only bumping newest post to the second on the list, not the top
+  comparator: function(a,b) {
+    return (new Date(b.get('created_at'))) - (new Date(a.get('created-at')));
+  }
 });
 
 export default {PostModel, PostsCollection};
